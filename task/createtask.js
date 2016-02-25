19ed28ed0afc3ba88375c59c8311182f6f79e6ce
lenagoren <lenagoren2018@u.northwestern.edu>
2016-02-25 17:04:55 -0600

    yooooooo create some tasks friends

---
+++ task/createtask.js
@@ -0,0 +1,26 @@
+
+
+//var inputs = new Array();
+
+
+
+/*var fields = document.getElementsByTagName('input');
+for (var i = 0; i < fields.length; i++) {
+    if (fields[i].getAttribute("required") != null) {
+        fields[i].style.border = '1px solid #ff0000';
+        requiredfields.push(fields[i]);
+    }
+}*/
+
+
+
+var submit = document.getElementById("submit_button");
+submit.onclick = function(e)  {
+	var fields = document.getElementById("exampletaskname").value;
+	var task = new Task(fields, document.getElementById("description").value, document.getElementById("volunteers").value, document.getElementById("deadline").value, document.getElementById("Events").value); 
+	console.log(task);
+	//window.alert(taskname)
+};
+
+
+
