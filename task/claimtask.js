46c50f3f44117f1482b8ec5fdadb4b37e1c7e0a0
lenagoren <lenagoren2018@u.northwestern.edu>
2016-02-28 21:59:47 -0600

    hopefully ok

---
+++ task/claimtask.js
@@ -0,0 +1,15 @@
+
+var task1claim = document.getElementById("task1");
+task1claim.onclick = function(e)  {
+	window.alert("YOOOOOO")
+	//var r = window.confirm("would you like to claim this task?");
+	//if (r == true) {
+	//	window.alert("you have successfully claimed this task")
+	//}
+
+
+	//var fields = document.getElementById("exampletaskname").value;
+	//var task = new Task(fields, document.getElementById("description").value, document.getElementById("volunteers").value, document.getElementById("deadline").value, document.getElementById("Events").value); 
+	//console.log(task);
+	//window.alert("Success " + JSON.stringify(task.task_name)+ " created!!!");
+};
